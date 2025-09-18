d308ac3b64491fc8c88721efce291bfb515bad90
alexbuga <alex.buga@gmail.com>
2025-09-18 13:52:36 +0300

    Added MockData class for namespacing

---
+++ Sources/Mocker/Mocker+MockedData.swift
@@ -0,0 +1,14 @@
+//
+//  MockedData.swift
+//  Mocker
+//
+//  Created by Alex Buga on 18.09.2025.
+//
+
+import Foundation
+
+public extension Mocker {
+    class MockedData {
+        static let example = "{\"mocked\": true}"
+    }
+}
